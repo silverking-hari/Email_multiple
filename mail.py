9e380c8ee3066bbd6e1a2309f5f1265f189cd5ef
silverking-hari <hairjordan95@gmail.com>
2023-03-05 21:10:10 +0530

    updated 5-3-23

--- mail.py
+++ mail.py
@@ -1,7 +1,5 @@
 import os
-import random
 import smtplib
-import ssl
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -25,35 +23,30 @@ password_email = os.getenv("PASSWORD")
 # setting variable for E-mail
 def calling_mail(receiver_email, name, subject, folder_path, random_file):
     # Create a multipart message object
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = (subject + ' {}'.format(name.upper()))
-    msg['From'] = sender
-    msg['To'] = receiver_email
+    msg = MIMEMultipart('related')
     # reading the file_content
     source = os.path.join(current_dir, folder_path)
     with open(os.path.join(source, random_file), 'r') as f:
-        content = f.read().split("==html==")
-        f.close()
-        text = content[0]
-        message_content = (content[1].replace('%s', name))
-
-        # Adding the image
-        image_path = current_dir / 'Templates' / 'images' / 'guinea-pig.jpg'
-        # image_list = os.listdir(image_path)
-        # image = random.choice(image_list)
-        with open(image_path, "rb") as img_data:
-            img = img_data.read()
-            img_data.close()
-        image = MIMEImage(img)
-        image.add_header('Content-ID', '<image1>')
-        msg.attach(image)
+        message_content = (f.read().replace('{}', name))
 
     # Add the plain text and HTML content to the message
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(message_content, 'html')
-    msg.attach(part1)
-    msg.attach(part2)
+    html = MIMEText(message_content, 'html')
+    msg.attach(html)
 
+    # Adding the image
+    image_path = current_dir / 'Templates' / 'images' / 'guinea-pig.jpg'
+    # image_list = os.listdir(image_path)
+    # image = random.choice(image_list)
+    with open(image_path, "rb") as f:
+        img_data = f.read()
+    image = MIMEImage(img_data)
+    image.add_header('Content-ID', '<image1>')
+    msg.attach(image)
+
+    #setting the parameters
+    msg['Subject'] = (subject + ' {}'.format(name.upper()))
+    msg['From'] = sender
+    msg['To'] = receiver_email
 
     # sending the email
     if server:
